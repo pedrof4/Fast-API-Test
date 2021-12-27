90d2d59878bf2aa0b81b1835e62d004b3711ba77
Pedro <pmcastrofranca09@gmail.com>
2021-12-27 19:54:41 -0300

    Update

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-# import ao contrario do js
+# importaçoes
 from fastapi import FastAPI
 from routes.student import student_router
-# criando aplicativo
+from fastapi.middleware.cors import CORSMiddleware
+
+# Our REACT app will be running on this IP and PORT
+client_apps = ['http://localhost:3000']
+
+# Crianção do aplicativo
 app = FastAPI()
-# registre sua rota
+# registrando a rota
 app.include_router(student_router)
+
+# Registra o app com o CORS permite compartilhamento de recursos de diferente dominios/origem
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
--- routes/student.py
+++ routes/student.py
@@ -4,7 +4,14 @@ from fastapi import APIRouter
 from models.student import Student
 from config.database import connection
 from schemas.student import studentEntity, listOfStudentEntry
+from bson import ObjectId
 student_router = APIRouter()
+
+
+@student_router.get('/hello')
+async def hello_world():
+    return "Hello world"
+
 
 # usar todos os students
 
@@ -13,6 +20,13 @@ student_router = APIRouter()
 async def find_all_student():
     return listOfStudentEntry(connection.local.student.find())
 
+# buscar por 1 student atraves do id
+
+
+@student_router.get('/students/{studentId}')
+async def find_student_by_id(studentId):
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+
 # criar um student
 
 
@@ -20,3 +34,21 @@ async def find_all_student():
 async def create_student(student: Student):
     connection.local.student.insert_one(dict(student))
     return listOfStudentEntry(connection.local.student.find)
+
+# atualizar um estudante
+
+
+@student_router.put('/students/{studentId}')
+async def update_student(studentId, student: Student):
+    # esta funçao encontra e atualiza o valor do student com novos dados
+    connection.local.student.find_one_and_update(
+        {"_id": ObjectId(studentId)},
+        {"$set": dict(student)}
+    )
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+# deletar estudante
+
+
+@student_router.delete('/students/{studentId}')
+async def delete_student(studentId):
+    return studentEntity(connection.local.student.find_one_and_delete({"_id": ObjectId(studentId)}))
